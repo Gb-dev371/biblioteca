65cd8e2dc60435ade7e4800d65aa8851245df1ba
gabriel.cferreira <gbdev371@gmail.com>
2023-08-24 11:45:53 -0300

    atualizado

--- src/Livro.java
+++ src/Livro.java
@@ -1,2 +1,90 @@
-package PACKAGE_NAME;public class Livro {
+import java.util.Scanner;
+
+public class Livro {
+    private String nome, autor, editora, prateleira, secao;
+    private int ISBN, edicao, ano, CDD;
+
+    public Livro(String nome, String autor, String editora, String prateleira, String secao, int ISBN, int edicao, int ano, int CDD) {
+        this.nome = nome;
+        this.autor = autor;
+        this.editora = editora;
+        this.prateleira = prateleira;
+        this.secao = secao;
+        this.ISBN = ISBN;
+        this.edicao = edicao;
+        this.ano = ano;
+        this.CDD = CDD;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public String getPrateleira() {
+        return prateleira;
+    }
+
+    public void setPrateleira(String prateleira) {
+        this.prateleira = prateleira;
+    }
+
+    public String getSecao() {
+        return secao;
+    }
+
+    public void setSecao(String secao) {
+        this.secao = secao;
+    }
+
+    public int getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public int getEdicao() {
+        return edicao;
+    }
+
+    public void setEdicao(int edicao) {
+        this.edicao = edicao;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public int getCDD() {
+        return CDD;
+    }
+
+    public void setCDD(int CDD) {
+        this.CDD = CDD;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,90 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner inp = new Scanner(System.in);
+        Livro[] livros = new Livro[1000];
+        String retorno, retorno2;
+        int i = 0;
+
+        do{
+            System.out.println("********************************");
+            System.out.println("Sistema de Cadastro de Livros:");
+            System.out.println("********************************");
+            System.out.println("1 - Cadastrar livro");
+            System.out.println("2 - Cadastrar revista");
+            System.out.println("3 - Procurar livro");
+            System.out.println("4 - Listar livros");
+            System.out.println("5 - Quantidade cadastrada/livre");
+            System.out.println("6 - Excluir último livro da lista");
+            System.out.println("7 - Ordenar livros cadastrados");
+            System.out.println("8 - Encerrar programa");
+
+            retorno = inp.next();
+
+            if(retorno.equals("1")){
+                String nome, autor, editora, prateleira, secao;
+                int ISBN, edicao, ano, CDD;
+
+                System.out.println("Informe o nome do livro: ");
+                nome = inp.next();
+
+                System.out.println("Informe o nome do autor: ");
+                autor = inp.next();
+
+                System.out.println("Informe o número do ISBN: ");
+                ISBN = inp.nextInt();
+
+                System.out.println("Informe o número da edição do livro: ");
+                edicao = inp.nextInt();
+
+                System.out.println("Informe o ano de publicação do livro: ");
+                ano = inp.nextInt();
+
+                System.out.println("Informe o nome da editora do livro: ");
+                editora = inp.next();
+
+                System.out.println("Informe a prateleira: ");
+                prateleira = inp.next();
+
+                System.out.println("Informe a seção: ");
+                secao = inp.next();
+
+                System.out.println("Informe o CDD: ");
+                CDD = inp.nextInt();
+
+                System.out.print("");
+                livros[i] = new Livro(nome, autor, editora, prateleira, secao, ISBN, edicao, ano, CDD);
+                // String nome, String autor, String editora, String prateleira, String secao, int ISBN, int edicao, int ano, int CDD
+
+                i++;
+                System.out.println("Livro cadastrado com sucesso!");
+            }
+
+            if(retorno.equals("4")){
+                System.out.println("Livros listados:");
+                for(int k=0; k<i; k++){
+                    System.out.println(livros[k].getNome() +", " +
+                            livros[k].getAutor() +", " +
+                            livros[k].getISBN() +", " +
+                            livros[k].getAno() +", " +
+                            livros[k].getEditora() +", " +
+                            livros[k].getPrateleira() +", "+
+                            livros[k].getSecao() +", "+
+                            livros[k].getISBN() +", "+
+                            livros[k].getEdicao() +", "+
+                            livros[k].getAno() +", "+
+                            livros[k].getCDD() +", ");
+                }
+            }
+
+            if(retorno.equals("5")){
+                System.out.println("Total = 1000");
+                System.out.println("Usado = "+i);
+                System.out.println("Disponíveis: "+(1000-i));
+            }
+
+        }while(!retorno.equals("7"));
+
+        System.out.println("Fim do programa");
     }
 }
